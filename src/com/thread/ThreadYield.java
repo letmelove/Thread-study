c68e59f6b6ec3683df7499d54fe0e096f870df1e
tx <txipromise@163.com>
2020-09-17 14:08:37 +0800

    使用yield方法礼让线程

---
+++ src/com/thread/ThreadYield.java
@@ -0,0 +1,24 @@
+package com.thread;
+
+/**
+ * 线程礼让
+ * 礼让不一定成功，还是看cup来调度
+ */
+public class ThreadYield {
+
+    public static void main(String[] args) {
+        MyYield my=new MyYield();
+        new Thread(my,"a").start();
+        new Thread(my,"b").start();
+    }
+
+}
+class MyYield implements Runnable{
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread().getName()+"---->开始执行");
+        //礼让
+        Thread.yield();
+        System.out.println(Thread.currentThread().getName()+"---->线程结束");
+    }
+}
